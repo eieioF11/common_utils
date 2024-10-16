5e4082ef1ec3e8ad2de58cf96e0218fd4d6bea60
f11 <eieioeiji0501@gmail.com>
2024-10-16 20:44:19 +0900

    [add]added eigen_utils function

--- utility/eigen_utils.hpp
+++ utility/eigen_utils.hpp
@@ -12,14 +12,43 @@
 #include <Eigen/Dense>
 
 namespace common_utils {
+  /**
+   * @brief vector -> Eigen::VectorX
+   *
+   * @tparam T
+   * @param vec std::vector<T>
+   * @return Eigen::VectorX<T>
+   */
   template <class T>
   constexpr inline Eigen::VectorX<T> set_vectorx(std::vector<T>& vec) {
     Eigen::Map<Eigen::VectorX<T>> p(vec.data(), vec.size());
     return p;
   }
+
+  /**
+   * @brief vector -> Eigen::VectorX
+   *
+   * @tparam T
+   * @param vec std::vector<T>
+   * @return Eigen::VectorX<T>
+   */
   template <class T>
   constexpr inline Eigen::VectorX<T> set_vectorx(std::vector<T> vec) {
     Eigen::Map<Eigen::VectorX<T>> p(vec.data(), vec.size());
     return p;
+  }
+
+  /**
+   * @brief approx_eq Eigen::VectorX version
+   *
+   * @tparam T
+   * @param  a Eigen::VectorX<T>
+   * @param  b Eigen::VectorX<T>
+   * @param  range T default:1e-12
+   * @return bool
+   */
+  template <class T>
+  constexpr inline bool approx_eq(const Eigen::VectorX<T>& a,const Eigen::VectorX<T>& b, T range = 1e-12) {
+    return (bool)((a - b).norm() < range);
   }
 } // namespace common_utils
