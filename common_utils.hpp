2ee4ba5d8279f5066928a65acf4cfec58bea01af
NishizawaEiji <nishizawa@aisl.cs.tut.ac.jp>
2024-10-17 14:57:51 +0900

    [update]update common_utils

--- common_utils.hpp
+++ common_utils.hpp
@@ -4,14 +4,18 @@
 #include "utility/string_utils.hpp"
 #include "utility/unit_utils.hpp"
 #include "utility/eigen_utils.hpp"
-// ext
-#include "ext/queue.hpp"
-#include "ext/timer.hpp"
 // ros2
 #ifdef USE_ROS2
 #include "ros2_utility/ros2_utils.hpp"
+#define USE_ROS2_OCCUPANCY_GRID
+#define USE_ROS2_NAV_PATH
 #endif
 // pcl
 #ifdef USE_PCL
 #include "pcl_utility/pcl_utils.hpp"
 #endif
+// ext
+#include "ext/queue.hpp"
+#include "ext/timer.hpp"
+#include "ext/grid_map.hpp"
+#include "ext/path.hpp"
